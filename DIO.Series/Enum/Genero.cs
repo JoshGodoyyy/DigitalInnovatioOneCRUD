8f5d84a34acad7002f79c9c8ed1b0222e6c62d8e
Josh <josueaugusto@live.com>
2021-03-21 20:49:18 -0300

    Projeto de CRUD desenvolvido em curso da Digital Innovation One

---
+++ DIO.Series/Enum/Genero.cs
@@ -0,0 +1,19 @@
+﻿namespace DIO.Series
+{
+    public enum Genero : int
+    {
+        Acao,
+        Aventura,
+        Comedia,
+        Documentario,
+        Drama,
+        Espionagem,
+        Faroeste,
+        Fantasia,
+        Ficcao_Cientifica,
+        Musical,
+        Romance,
+        Suspense,
+        Terror
+    }
+}
